523da6362f085c2e8a29e9d35fe041dc5ef072e5
shaomingquan <shaomingquan@bytedance.com>
2018-09-04 21:57:52 +0800

    some pm2 thing

--- index.js
+++ index.js
@@ -9,33 +9,51 @@ const logger = require('koa-logger')
 const bodyParser = require('koa-bodyparser');
 
 module.exports = function (options) {
-  if (options.mode == 'dev') {
-    app.use(logger())
-  }
-  app.use(bodyParser())
 
-  router.get('/', serve('./public'))
-
-  const routerDefine = require('./apis')
-  const routers = Object.keys(routerDefine)
-  for (const defineName of routers) {
-    const {
-      method,
-      controller,
-      path: _path
-    } = routerDefine[defineName]
-    let path = _path
-    if (!path) {
-      path = defineName
+    if (options.mode == 'development') {
+        app.use(logger())
+    } else if (options.mode == 'production') {
+        process.on('SIGINT', function () {
+            // clear
+            console.log('graceful stop')
+            process.exit(0)
+        })
     }
-    if (method && controller) {
-      router[method.toLowerCase()](`/api/${path}`, controller)
+    app.use(bodyParser())
+
+    router.get('/', serve('./public'))
+
+    const routerDefine = require('./apis')
+    const routers = Object.keys(routerDefine)
+    for (const defineName of routers) {
+        const {
+            method,
+            controller,
+            path: _path
+        } = routerDefine[defineName]
+        let path = _path
+        if (!path) {
+            path = defineName
+        }
+        if (method && controller) {
+            router[method.toLowerCase()](`/api/${path}`, controller)
+        }
     }
-  }
 
-  app
-    .use(router.routes())
-    .use(router.allowedMethods())
+    app
+        .use(router.routes())
+        .use(router.allowedMethods())
 
-  app.listen(options.port)
+    if (options.mode == 'development') {
+        app.listen(options.port, _ => {
+            console.log("server started")
+        })
+    } else if (options.mode == 'production') {
+        app.listen(options.port, () => {
+            console.log("server started")
+            process.send('ready')
+        })
+    }
+    
+
 }
